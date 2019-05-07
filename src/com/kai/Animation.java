df5c72421f5f7ec69e141ff7563d5a015fd18263
Kai <tstarkflyingman@gmail.com>
2019-05-07 13:36:31 -0700

    documentation

--- src/com/kai/Animation.java
+++ src/com/kai/Animation.java
@@ -2,62 +2,176 @@ package com.kai;
 
 import java.awt.image.BufferedImage;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
 /**
- * Created: 05/03/2019
- * Author: Kai Tinkess
+ * A class representing an animation, or a
+ * collection of image frames underneath a title.
+ *
+ * @author Kai Tinkess on May 3rd, 2019
  */
 
 public class Animation {
 
-    //The name/title of the animation.
+    /**
+     * The title/name of the animation.
+     */
     private String title;
-    //A collection of every frame in the animation
+    /**
+     * A collection of every frame in the animation.
+     */
     private BufferedImage[] frames;
 
+    /**
+     * Creates an animation with a title and passed in array of frames.
+     *
+     * @param title The title assigned to the anim.
+     * @param frames An array of BufferedImages with each being a single frame.
+     */
     public Animation(String title, BufferedImage[] frames) {
         this.title = title;
         this.frames = frames;
     }
 
+    /**
+     * Creates the animation with a default title and the given frame list.
+     *
+     * @see Animation#Animation(String, BufferedImage[])
+     */
     public Animation(BufferedImage[] frames) {
         this("untitled", frames);
     }
 
-    public Animation(String title, List<BufferedImage> frames) {
+    /**
+     * Creates the animation from the given title and collection of frames.
+     * The collection is converted to a primitive array internally.
+     *
+     * @see Animation#Animation(String, BufferedImage[])
+     */
+    public Animation(String title, Collection<BufferedImage> frames) {
         this(title, (BufferedImage[]) frames.toArray());
     }
 
+    /**
+     * Creates the animation with a default title and the given collection of frames.
+     *
+     * @see Animation#Animation(List)
+     */
     public Animation(List<BufferedImage> frames) {
         this("untitled", (BufferedImage[]) frames.toArray());
     }
 
+    /**
+     * Creates an animation with the data in the given animation.
+     *
+     * @param anim The animation that is being copied.
+     */
     public Animation(Animation anim) {
         this.title = anim.getTitle();
         this.frames = anim.getFrames();
     }
 
-    //Returns a specific frame.
+    /**
+     * Creates an animation with a default title and an empty string array.
+     *
+     * @see Animation#Animation(String, BufferedImage[])
+     */
+    public Animation() {
+        this("untitled", new BufferedImage[] {});
+    }
+
+    /**
+     * Get a single frame of the frame array.
+     *
+     * @param index The specific index of a single frame in the array of images.
+     * @return The frame listed at that index.
+     */
     public BufferedImage getFrame(int index) {
         return frames[index];
     }
 
-    //Returns the title of the animation.
+    /**
+     * Get the title of this animation.
+     *
+     * @return This animation's title.
+     */
     public String getTitle() {
         return title;
     }
 
-    //Returns the animation's frames in an array.
+    /**
+     * Get the frames of this animation.
+     *
+     * @return The animation's frames, held in a primitive array.
+     */
     public BufferedImage[] getFrames() {
         return frames;
     }
 
-    //Returns how many frames the animation has.
+    /**
+     * Set the title of the animation.
+     *
+     * @param title The new title of the animation.
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Sets the frame list of the animation.
+     *
+     * @param frames The new array of frames.
+     */
+    public void setFrames(BufferedImage[] frames) {
+        this.frames = frames;
+    }
+
+    /**
+     * Sets a single frame in the animation to a new image.
+     *
+     * @param newFrame A new frame to replace one in the animation.
+     * @param index The index of the frame that is replaced.
+     */
+    public void setFrame(BufferedImage newFrame, int index) {
+        frames[index] = newFrame;
+    }
+
+    /**
+     * Adds a single frame into a specified index in the frame array.
+     * Index can be one greater than the existing array length.
+     *
+     * @param newFrame The new image to be inserted into the array.
+     * @param index The index the new frame will be inserted at.
+     * @return A boolean representing if the insertion was successful.
+     */
+    public boolean addFrame(BufferedImage newFrame, int index) {
+        if (index > getFrameCount() || index < 0) return false;
+
+        BufferedImage[] array = new BufferedImage[getFrameCount()+1];
+        boolean located = false;
+        for (int i = 0; i < frames.length; i++) {
+            if (i == index) {
+                array[i] = newFrame;
+                located = true;
+            }
+            array[i] = frames[i - ((located) ? 1 : 0)];
+        }
+        frames = array;
+
+        return true;
+    }
+
+    /**
+     * Gets the length of the frame array.
+     *
+     * @return The number of frames in this animation.
+     */
     public int getFrameCount() {
         return frames.length;
     }
+
 
     @Override
     public String toString() {
--- src/com/kai/AnimationPlayer.java
+++ src/com/kai/AnimationPlayer.java
@@ -1,43 +1,95 @@
 package com.kai;
 
-//TODO: Revisit and rewrite with proper JavaDoc.
-
 import java.awt.image.BufferedImage;
 import java.util.*;
 
 /**
- * Created: 05/03/2019
- * Author: Kai Tinkess
+ * An AnimationPlayer meant to be used in your base level "Entity" class.
+ * In the update/render loop for the containing class, drawing nextFrame(), on
+ * the condition that it is not null will allow freedom over animations.
+ * Allows the user to set a repeating animation, useful for running or consistent animations.
+ * Allows the user to queue animations in order to have animations play in a certain order.
+ * Allows the user to force play animations that bypass the queue.
+ *
+ * @see AnimationPlayer#setRepeatingAnim(String)
+ * @see AnimationPlayer#queueAnim(String)
+ * @see AnimationPlayer#playAnim(String)
+ *
+ * @author Kai Tinkess on May 3rd, 2019
  */
 
 public class AnimationPlayer {
 
-    //Represents the number of frames displayed per second.
-    //If playing an anim with 5 frames, and while being called 10 times per second, each frame is returned twice.
+    /**
+     * Represents the number of frames displayed per second.
+     * If playing an anim with 5 frames,
+     * and while being called 10 times per second, each frame is returned twice.
+     */
     private int framesPerSecond = 5;
 
-    //A map of all anims currently in the animation player.
+    /**
+     * A map of all anims currently in the animation player.
+     * The key is the same title as its matching animation's title.
+     */
     private HashMap<String, Animation> animations = new HashMap<>();
 
-    //The time of the last time the current frame changed
+    /**
+     * The last time the frame given by nextFrame() changed.
+     * Always set to a value produced by System.currentTimeMillis();
+     *
+     * @see System#currentTimeMillis()
+     */
     private long lastFrameChange = -1;
 
-    //The current frame inside the currently playing anim.
+    /**
+     * The current frame that is being played inside of the current animation.
+     */
     private int currentFrame = 0;
 
-    //The anim that is currently playing.
+    /**
+     * The animation that is currently being played.
+     */
     private Animation currentAnim;
 
-    //Represents whether or not the player is currently playing.
+    /**
+     * Usually, but not always, synonymous with whether or not currentAnim is null.
+     * Represents whether or not next an anim (besides repeatingAnim) is being played.
+     */
     private boolean playing = false;
 
-    //The current queue of anims to be played.
+    /**
+     * The current queue of animations to be played after currentAnim ends.
+     * All animations in the queue are played before any repeatingAnim.
+     */
     private Queue<Animation> animQueue = new LinkedList<>();
 
-    //An optional repeating anim that is returned when no animations are being played.
+    /**
+     * Null by default.
+     * If set, plays infinitely whenever no other animations are queued or playing.
+     */
     private Animation repeatingAnim;
 
-    //Returns the next frame in the anim current being played.
+    /**
+     * Creates an AnimationPlayer from a pre-existing collection of animations.
+     *
+     * @param animations The collection of animations that is copied into this object.
+     */
+    public AnimationPlayer(Collection<Animation> animations) {
+        animations.forEach((anim) -> addAnim(anim));
+    }
+
+    /**
+     * Creates an empty AnimationPlayer.
+     */
+    public AnimationPlayer() {}
+
+    /**
+     * Returns the current frame of the current animation.
+     * If it has been "long enough" (1000.0/framesPerSecond), it updates the current frame.
+     * If there are no more frames, the next animation in the queue or the repeating animation are played.
+     *
+     * @return The current frame of the current animation.
+     */
     public BufferedImage nextFrame() {
 
         long currentTime = System.currentTimeMillis();
@@ -69,8 +121,13 @@ public class AnimationPlayer {
         return currentAnim.getFrame(currentFrame);
     }
 
-
-    //Optionally clears the current queue and overwrites the current playing animation to play the requested anim.
+    /**
+     * "Force" plays an animation, playing immediately and before anything in the queue.
+     * Optionally clears the current queue.
+     *
+     * @param anim The animation to play.
+     * @param clear A flag signalling whether or not to clear the current queue.
+     */
     public void playAnim(Animation anim, boolean clear) {
         if (clear) clearAnimQueue();
         resetFieldsForNextAnim();
@@ -78,39 +135,62 @@ public class AnimationPlayer {
         currentAnim = new Animation(anim);
     }
 
+    /**
+     * Clears the queue and plays an animation from the HashMap animations.
+     *
+     * @see AnimationPlayer#playAnim(Animation, boolean)
+     */
     public void playAnim(String title, boolean clear) {
         playAnim(animations.get(title), clear);
     }
 
+    /**
+     * @see AnimationPlayer#playAnim(Animation, boolean)
+     */
     public void playAnim(String title) {
         playAnim(animations.get(title), true);
     }
 
+    /**
+     * @see AnimationPlayer#playAnim(Animation, boolean)
+     */
     public void playAnim(Animation anim) {
         playAnim(anim, true);
     }
 
-
-    //Queues up an anim to play after currently queued anims are played.
+    /**
+     * Adds an animation to the queue, which is played after all existing queued animations are played.
+     *
+     * @param anim The animation to add to the queue.
+     */
     public void queueAnim(Animation anim) {
         animQueue.add(anim);
     }
 
+    /**
+     * Adds an animation to the queue from the animations HashMap.
+     *
+     * @see AnimationPlayer#queueAnim(Animation)
+     */
     public void queueAnim(String title) {
         queueAnim(animations.get(title));
     }
 
-    //Returns if the queue is empty or not.
+    /**
+     * Returns if the animation queue is empty or not.
+     *
+     * @return A boolean signaling whether or not the queue is empty.
+     */
     public boolean isAnimQueueEmpty() {
         return animQueue.isEmpty();
     }
 
-    //Clears the current queue of anims.
-    //Optionally stops playing the current anim.
-    public void clearAnimQueue() {
-        clearAnimQueue(false);
-    }
-
+    /**
+     * Clears the current queue of animations.
+     * Optionally stops playing the current animation.
+     *
+     * @param stopPlaying Signals whether or not to stop playing the current animation.
+     */
     public void clearAnimQueue(boolean stopPlaying) {
         animQueue.clear();
         if (stopPlaying) {
@@ -119,8 +199,19 @@ public class AnimationPlayer {
         }
     }
 
-    //Stops the anim from playing and clears the queue.
-    //Removes the repeating anim.
+    /**
+     * Clears the animation queue.
+     *
+     * @see AnimationPlayer#clearAnimQueue(boolean)
+     */
+    public void clearAnimQueue() {
+        clearAnimQueue(false);
+    }
+
+    /**
+     * Completely stops the player from playing.
+     * Clears the animation queue and removes the repeating animation.
+     */
     public void stop() {
         clearAnimQueue();
         playing = false;
@@ -128,57 +219,110 @@ public class AnimationPlayer {
         removeRepeatingAnim();
     }
 
-    //Resets the existing anim list and stops the player.
+    /**
+     * Stops the player from playing.
+     * Clears the animation map.
+     *
+     * @see AnimationPlayer#stop()
+     */
     public void reset() {
         stop();
         animations.clear();
     }
 
-    //Helper methods that sets the fields specific per anim.
-    public void resetFieldsForNextAnim() {
+    /**
+     * A helper method used to reset any animation-specific fields to a default state.
+     */
+    private void resetFieldsForNextAnim() {
         currentFrame = 0;
         playing = true;
     }
 
-    //Adds an anim to the map of anims held by this player.
-    //Returns true if the add request was successful.
-    public boolean addAnim(String title, BufferedImage[] frames) {
-        return addAnim(new Animation(title, frames));
-    }
-
-    public boolean addAnim(String title, List<BufferedImage> frames) {
-        return addAnim(new Animation(title, frames));
-    }
-
+    /**
+     * Adds an animation to the existing list of animations - duplicate or empty animations are not permitted.
+     * Sets the key for that animation to it's title.
+     * Returns a value indicating whether or not it was successful.
+     *
+     * @param anim The animation to be added.
+     * @return A boolean signaling if the animation was added successfully.
+     */
     public boolean addAnim(Animation anim) {
         //Duplicate anims or anims of 0 length are not permitted.
         if (containsAnim(anim.getTitle())) return false;
         if (anim.getFrameCount() == 0) return false;
-        if (anim == null) return false;
 
         animations.put(anim.getTitle(), anim);
         return true;
     }
 
+    /**
+     * Creates an animation and adds it to the animation list.
+     *
+     * @see AnimationPlayer#addAnim(Animation)
+     */
+    public boolean addAnim(String title, BufferedImage[] frames) {
+        return addAnim(new Animation(title, frames));
+    }
 
-    //Returns true if the animation player has an anim with the inputted title.
+    /**
+     * Creates an animation and adds it to the animation list.
+     *
+     * @see AnimationPlayer#addAnim(Animation)
+     */
+    public boolean addAnim(String title, List<BufferedImage> frames) {
+        return addAnim(new Animation(title, frames));
+    }
+
+    /**
+     * Adds a collection of animations to the animation map by calling addAnim() on each.
+     * Returns a value indicating if any of the add calls failed.
+     *
+     * @see AnimationPlayer#addAnim(Animation)
+     *
+     * @param animList The collection of animations to be added.
+     * @return A boolean signaling if any of the animations could not be added.
+     */
+    public boolean addAnims(Collection<Animation> animList) {
+        for (Animation anim: animList) {
+            boolean added = addAnim(anim);
+            if (!added) return false;
+        }
+        return true;
+    }
+
+    /**
+     * Returns whether or not the passed in title is a key in the animation map.
+     *
+     * @param title The title to search for in the animation map.
+     */
     public boolean containsAnim(String title) {
         return animations.keySet().contains(title);
     }
 
-    //Retrieves the frames per second.
+    /**
+     * Gets the frames per second of this AnimationPlayer.
+     *
+     * @return The frames per second.
+     */
     public int getFramesPerSecond() {
         return framesPerSecond;
     }
 
-    //Sets the frames per second.
+    /**
+     * Sets the frames of the AnimationPlayer.
+     *
+     * @param framesPerSecond The new frames per second.
+     */
     public void setFramesPerSecond(int framesPerSecond) {
         this.framesPerSecond = framesPerSecond;
     }
 
-
-    //Sets the repeating animation.
-    //Overrides the current repeating anim. but not a queued or played anim.
+    /**
+     * Sets the repeating animation of the player - the animation played when nothing is queued.
+     * Interrupts the current repeating anim, but not a queued or played one.
+     *
+     * @param anim The animation to be repeated.
+     */
     public void setRepeatingAnim(Animation anim) {
         Animation newRepeatingAnim = new Animation(anim);
         if (currentAnim == repeatingAnim) {
@@ -188,17 +332,39 @@ public class AnimationPlayer {
         repeatingAnim = newRepeatingAnim;
     }
 
+    /**
+     * Sets the repeating animation with an animation in the player's animation map.
+     *
+     * @see AnimationPlayer#setRepeatingAnim(Animation)
+     */
     public void setRepeatingAnim(String title) {
         setRepeatingAnim(animations.get(title));
     }
 
+    /**
+     * Removes the repeating animation.
+     */
     public void removeRepeatingAnim() {
         repeatingAnim = null;
     }
 
-    //Returns the specified anim
+    /**
+     * Returns a specific animation from the animation map.
+     *
+     * @param title The title of the desired animation.
+     * @return The value of the animation map at that key.
+     */
     public Animation getAnim(String title) {
         return animations.get(title);
+    }
+
+    /**
+     * Returns the values of the animation hash map.
+     *
+     * @return A collection of all animations added to the player.
+     */
+    public Collection<Animation> getAnims() {
+        return animations.values();
     }
 
 }
